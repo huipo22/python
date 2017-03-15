d7aa9967857419cff5e3b8eb8bf10f8be47ac549
huipo22 <huipo22@163.com>
2017-03-15 11:09:51 +0800

    page_source

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ webBrowser = webdriver.Chrome()
 print 'run ...'
 webBrowser.get('https://scienceblog.com/492785/castration-resistant-prostate-cancer-cell-growth-impeded-endostatin/')
 print 'get start ...'
-con=webBrowser.find_elements_by_xpath('//div[@class="entry-content"]//p')
-for i in con:
-    print i.text
+print webBrowser.page_source
+# con=webBrowser.find_elements_by_xpath('//div[@class="entry-content"]//p')
+# for i in con:
+#     print i.text
 print 'get over ........'
